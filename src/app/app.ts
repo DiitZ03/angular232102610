7734428e93adb4ad5c78214f25077b0f599434eb
DiitZ03 <pratamaaditya1254@gmail.com>
2025-11-03 10:35:26 +0700

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit{
   protected readonly title = signal('angular232102610');
+
+  constructor(private router : Router) {}
+
+  ngAfterViewInit(): void {
+    const body = document.body;
+    const toggleButton = document.querySelector('[data-widget="pushmenu"]');
+    const sidebarLinks = document.querySelectorAll('.main-sidebar a.nav-link');
+
+    // Klik tombol buka/tutup sidebar
+    if (toggleButton) {
+      toggleButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        body.classList.toggle('sidebar-open');
+      });
+    }
+
+    // Tutup otomatis jika user klik menu di mobile
+    sidebarLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        if (window.innerWidth <= 991 && body.classList.contains('sidebar-open')) {
+          body.classList.remove('sidebar-open');
+        }
+      });
+    });
+
+    // Tutup otomatis setiap kali ganti halaman (navigasi router)
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && window.innerWidth <= 991) {
+        body.classList.remove('sidebar-open');
+      }
+    });
+  }
 }
