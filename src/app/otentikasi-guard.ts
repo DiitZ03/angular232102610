9855d9fc3742a209086042af5f66b8b9b394780f
DiitZ03 <pratamaaditya1254@gmail.com>
2025-12-01 01:02:34 +0700

    commit

--- src/app/otentikasi-guard.ts
+++ src/app/otentikasi-guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
-
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 export const otentikasiGuard: CanActivateFn = (route, state) => {
   return true;
+  console.log("Otentikasi dimulai");
+
+  var userId = inject(CookieService).get('userId');
+  console.log("userId: " , userId);
+
+  if (userId == null) {
+    // Anggap belum login
+    else if (userId == "undenfined" ) {
+  }
+     {
 };
